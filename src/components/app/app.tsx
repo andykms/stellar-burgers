bfca68d9b9435ca125c88219ef2f0d60574e918d
Andrey Isaev <andrey.isaev.54.ru@gmail.com>
2025-06-04 21:06:56 +0300

    feat: add react-router

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,14 +1,23 @@
 import { ConstructorPage } from '@pages';
 import '../../index.css';
 import styles from './app.module.css';
-
+import { Routes, Route } from 'react-router-dom';
 import { AppHeader } from '@components';
 
-const App = () => (
-  <div className={styles.app}>
-    <AppHeader />
-    <ConstructorPage />
-  </div>
-);
+const App = () => {
+  return (
+    <Routes>
+      <Route
+        path={'/'}
+        element={
+          <div className={styles.app}>
+            <AppHeader />
+            <ConstructorPage />
+          </div>
+        }
+      />
+    </Routes>
+  );
+};
 
 export default App;
