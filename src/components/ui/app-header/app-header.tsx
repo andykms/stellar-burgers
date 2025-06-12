53418bbc28510563cbb635ecb7c0b0ba9b5a6a44
Andrey Isaev <andrey.isaev.54.ru@gmail.com>
2025-06-12 12:16:02 +0300

    refactor: add type for Icons in Header in current Page

--- src/components/ui/app-header/app-header.tsx
+++ src/components/ui/app-header/app-header.tsx
@@ -14,47 +14,71 @@ import clsx from 'clsx';
 const PATHS = {
   CONSTRUCTOR: '/',
   FEED: '/feed',
+  PROFILE: '/profile'
 };
-
 
 export const AppHeaderUI: FC<TAppHeaderUIProps> = ({ userName }) => {
   const location = useLocation();
   const currentPath = location.pathname;
-  console.log(currentPath, PATHS.CONSTRUCTOR);
-  return (<header className={styles.header}>
-    <nav className={`${styles.menu} p-4`}>
-      <div className={styles.menu_part_left}>
-        <>
-          <BurgerIcon type={'primary'} />
-          <Link
-            className={clsx(`text text_type_main-default ml-2 mr-10 ${styles.link}`, currentPath === PATHS.CONSTRUCTOR ? styles.link_active : '')}
-            to={'/'}
-          >
-            Конструктор
-          </Link>
-        </>
-        <>
-          <ListIcon type={'primary'} />
-          <Link
-            className={clsx(`text text_type_main-default ml-2 ${styles.link}`, currentPath === PATHS.FEED ? styles.link_active : '')}
-            to={'/feed'}
-          >
-            Лента заказов
-          </Link>
-        </>
-      </div>
-      <Link className={styles.logo} to={'/'}>
-        <Logo className='' />
-      </Link>
-      <div className={styles.link_position_last}>
-        <ProfileIcon type={'primary'} />
-        <Link
-          className={`text text_type_main-default ml-2 ${styles.link}`}
-          to='/profile'
-        >
-          {userName || 'Личный кабинет'}
+
+  let linkActiveStyle = '';
+
+  const ActiveLinks = {
+    activeConstructor:
+      currentPath === PATHS.CONSTRUCTOR ? styles.link_active : null,
+    activeFeed: currentPath === PATHS.FEED ? styles.link_active : null,
+    activeProfile: currentPath === PATHS.PROFILE ? styles.link_active : null
+  };
+
+  return (
+    <header className={styles.header}>
+      <nav className={`${styles.menu} p-4`}>
+        <div className={styles.menu_part_left}>
+          <>
+            <BurgerIcon
+              type={ActiveLinks.activeConstructor ? 'primary' : 'secondary'}
+            />
+            <Link
+              className={clsx(
+                `text text_type_main-default ml-2 mr-10 ${styles.link}`,
+                ActiveLinks.activeConstructor
+              )}
+              to={'/'}
+            >
+              Конструктор
+            </Link>
+          </>
+          <>
+            <ListIcon type={ActiveLinks.activeFeed ? 'primary' : 'secondary'} />
+            <Link
+              className={clsx(
+                `text text_type_main-default ml-2 ${styles.link}`,
+                ActiveLinks.activeFeed
+              )}
+              to={'/feed'}
+            >
+              Лента заказов
+            </Link>
+          </>
+        </div>
+        <Link className={styles.logo} to={'/'}>
+          <Logo className='' />
         </Link>
-      </div>
-    </nav>
-  </header>)
+        <div className={styles.link_position_last}>
+          <ProfileIcon
+            type={ActiveLinks.activeProfile ? 'primary' : 'secondary'}
+          />
+          <Link
+            className={clsx(
+              `text text_type_main-default ml-2 ${styles.link}`,
+              ActiveLinks.activeProfile
+            )}
+            to='/profile'
+          >
+            {userName || 'Личный кабинет'}
+          </Link>
+        </div>
+      </nav>
+    </header>
+  );
 };
