0f89ce4d595939932e4d5d37d42b873e96889f73
Andrey Isaev <andrey.isaev.54.ru@gmail.com>
2025-06-12 11:50:59 +0300

    refactor: add style for link in current page

--- src/components/ui/app-header/app-header.tsx
+++ src/components/ui/app-header/app-header.tsx
@@ -8,15 +8,26 @@ import {
   ProfileIcon
 } from '@zlden/react-developer-burger-ui-components';
 import { Link } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
+import clsx from 'clsx';
 
-export const AppHeaderUI: FC<TAppHeaderUIProps> = ({ userName }) => (
-  <header className={styles.header}>
+const PATHS = {
+  CONSTRUCTOR: '/',
+  FEED: '/feed',
+};
+
+
+export const AppHeaderUI: FC<TAppHeaderUIProps> = ({ userName }) => {
+  const location = useLocation();
+  const currentPath = location.pathname;
+  console.log(currentPath, PATHS.CONSTRUCTOR);
+  return (<header className={styles.header}>
     <nav className={`${styles.menu} p-4`}>
       <div className={styles.menu_part_left}>
         <>
           <BurgerIcon type={'primary'} />
           <Link
-            className={`text text_type_main-default ml-2 mr-10 ${styles.link}`}
+            className={clsx(`text text_type_main-default ml-2 mr-10 ${styles.link}`, currentPath === PATHS.CONSTRUCTOR ? styles.link_active : '')}
             to={'/'}
           >
             Конструктор
@@ -25,7 +36,7 @@ export const AppHeaderUI: FC<TAppHeaderUIProps> = ({ userName }) => (
         <>
           <ListIcon type={'primary'} />
           <Link
-            className={`text text_type_main-default ml-2 ${styles.link}`}
+            className={clsx(`text text_type_main-default ml-2 ${styles.link}`, currentPath === PATHS.FEED ? styles.link_active : '')}
             to={'/feed'}
           >
             Лента заказов
@@ -45,5 +56,5 @@ export const AppHeaderUI: FC<TAppHeaderUIProps> = ({ userName }) => (
         </Link>
       </div>
     </nav>
-  </header>
-);
+  </header>)
+};
